24efea0554736694f2714e0bfaaf037c9568b5f9
Sergey Khlobostin <hlobser@gmail.com>
2023-07-17 16:12:38 +0300

    добавил время, экран, пищалку

--- autopoliv/autopoliv.ino
+++ autopoliv/autopoliv.ino
@@ -1,15 +1,16 @@
 #include "iarduino_RTC.h"
-iarduino_RTC time(RTC_DS1302,6,8,7);  // для модуля DS1302 - RST, CLK, DAT
-                                      //у экрана scl к A5, sda к A4
-
 #include "Wire.h"                     // библиотека для устройств I2C 
 #include "LiquidCrystal_I2C.h"        // подключаем библиотеку для дисплея
-LiquidCrystal_I2C LCD(0x27,16,2);    // присваиваем имя дисплею
+#include "SPI.h"
+#include "SD.h"
 
+LiquidCrystal_I2C LCD(0x27, 16, 2);    // присваиваем имя дисплею
+iarduino_RTC time(RTC_DS1302, 6, 7, 8);  // для модуля DS1302 - RST, CLK, DAT
+                                      //у экрана scl к A5, sda к A4
 
 #define pumpPin  2        // Пин, к которому подключено реле насоса
 #define soilSensorPin  A0 // Аналоговый пин, к которому подключен датчик почвы
-#define piezoPin 3   // пин пьезодинамика
+#define piezoPin 3        // пин пьезодинамика
 
 
 // Пороговые значения для полива и остановки полива
@@ -21,63 +22,74 @@ LiquidCrystal_I2C LCD(0x27,16,2);    // присваиваем имя диспл
 unsigned int soilMoisture;
 
 void setup() {
+  delay(300);
   // Инициализация пина насоса как выходного
   pinMode(pumpPin, OUTPUT);
   pinMode(piezoPin, OUTPUT);
+  tone(piezoPin, 1000, 100);
+  delay(200);
+  tone(piezoPin, 2000, 100);
+  delay(200);
+  tone(piezoPin, 3000, 100);
 
   LCD.init();            // инициализация LCD дисплея
-  LCD.backlight();      // включение подсветки дисплея
+  // LCD.backlight();      // включение подсветки дисплея
+  LCD.setCursor(0, 0);
+  LCD.print("Humidity:");
+  LCD.setCursor(0, 1);
+  LCD.print("data and time");
+
   time.begin();
-  
-  // Инициализация последовательной связи с монитором порта
+  //time.settime(0, 13, 14, 17, 7, 23, 2);  // 0  сек, 13 мин, 14 часов, 17, июля, 2023, понедельник
   Serial.begin(9600);
   digitalWrite(pumpPin, HIGH);
   delay(40000);
+  tone(piezoPin, 3000, 100);
+  delay(200);
+  tone(piezoPin, 2000, 100);
+  delay(200);
+  tone(piezoPin, 1000, 100);
+  
 }
 // Функция для включения полива
   
 void startWatering() {
   digitalWrite(pumpPin, LOW);  // Включаем реле насоса
   Serial.println("Полив начат");
-  for (int x = 1000; x <= 2000 ; x += 500){ // пищим что полив начался
-    tone (piezoPin, x, 50);
-     
-  }
-  // tone(piezoPin, 1000, 50);
-  // tone(piezoPin, 1500, 50);
-  // tone(piezoPin, 2000, 50);
-  delay(5000);
+  tone(piezoPin, 1000, 100);
+  delay(200);
+  tone(piezoPin, 2000, 100);
+  delay(200);
+  tone(piezoPin, 3000, 100);
+  
+}
+// Функция для остановки полива
+void stopWatering() {
   digitalWrite(pumpPin, HIGH);  // Выключаем реле насоса
   Serial.println("Полив остановлен");
-  for (int x = 2000; x >= 1000 ; x +=500){ // пищим что полив закончился
-    tone (piezoPin, x, 50);
-  }
-  // tone(piezoPin, 2000, 50);
-  // tone(piezoPin, 1500, 50);
-  // tone(piezoPin, 1000, 50);
+  tone(piezoPin, 3000, 100);
+  delay(200);
+  tone(piezoPin, 2000, 100);
+  delay(200);
+  tone(piezoPin, 1000, 100);
+}
+void printToScreen(){
+  LCD.setCursor(9, 0);
+  LCD.print(soilMoisture);
+  LCD.setCursor(0, 1);
+  LCD.print(time.gettime("d-m, H:i:s"));
+}
+void writeToSD(){
 
 }
-  
-// Функция для остановки полива
-// void stopWatering() {
-//   digitalWrite(pumpPin, HIGH);  // Выключаем реле насоса
-//   Serial.println("Полив остановлен");
-// }
-
 void loop() {
   // Считываем значение с датчика почвы
   soilMoisture = analogRead(soilSensorPin);
-  //soilMoisture = map(soilMoisture, MIN, MAX, 0, 100);
 
-  if (millis() % 1000 == 0) {
-    LCD.setCursor(0,0);                     // 0 столбец, 0 строка
-    LCD.print(time.gettime("d M Y, D"));
-    LCD.setCursor(0,1);                     // 0 столбец, 1 строка
-    LCD.print(time.gettime("H:i:s"));
-  }
   // Выводим значение в монитор порта
   Serial.print("Влажность почвы: ");
   Serial.println(soilMoisture);
+  Serial.println(time.gettime("d-m, H:i:s"));
 
   // Проверяем условия полива
   // if (soilMoisture < dryThreshold) {
@@ -85,13 +97,14 @@ void loop() {
   // }  
   if (soilMoisture > moistureThreshold) {
     startWatering();
+    printToScreen();
+    delay(5000);
+    stopWatering();
+
   }
 
   // Задержка между измерениями
   delay(1800000);
+  // delay(10000);
 }
 
-// Функция для получения значения влажности почвы
-//long getSoilMoisture() {
- // long moistureValue = soilSensor.capacitiveSensor(30); // Используем метод capacitiveSensor() для получения значения
- // return moistureValue;
